e430ab6a028125a25ac2b4e859be9dc03c766bba
chrisbmx20720 <christian.al.ac20@gmail.com>
2026-08-20 21:32:01 -0600

    first commit

--- index.js
+++ index.js
@@ -1,30 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+
 const reportRoutes = require("./routes/reportRoutes");
 
 const app = express();
-const PORT = 5000;
 
-// CORS
-app.use(
-  cors({
-    origin: "http://localhost:5173"
-  })
-);
-
-// Parse JSON
+app.use(cors());
 app.use(express.json());
 
-// Test route
+app.use("/api/reports", reportRoutes);
+
 app.get("/", (req, res) => {
   res.json({
-    message: "Elite Marine Stabilizers API is running"
+    message: "Elite Marine WorkLog API",
+    status: "online"
   });
 });
 
-// Report routes
-app.use("/api/reports", reportRoutes);
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+module.exports = app;
